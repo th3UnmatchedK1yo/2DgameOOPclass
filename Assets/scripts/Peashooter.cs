d5ea443577e715abf08956e0cdbdb60195169db4
Zero Y One K <markbillhv@gmail.com>
2024-12-01 20:40:49 +0700

    create peashooter enemy and bullet prefab

---
+++ Assets/scripts/Peashooter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peashooter : MonoBehaviour
+{
+    //shoot
+    public bool isShootable = false;
+    public GameObject bullet;
+    public float bulletSpeed;
+    public float timeBtwFire;
+    private float fireCooldown;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireCooldown -= Time.deltaTime;
+        if(fireCooldown < 0)
+        {
+            fireCooldown = timeBtwFire;
+            //shoot
+            EnemyFireBullet();
+        }
+        
+    }
+
+    void EnemyFireBullet()
+    {
+        var bulletTmp = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        Vector3 playerPos = FindObjectOfType<PlayerMovement>().transform.position;
+        Vector3 direction = playerPos - transform.position;
+        rb.AddForce(direction.normalized * bulletSpeed, ForceMode2D.Impulse);
+    }
+}
