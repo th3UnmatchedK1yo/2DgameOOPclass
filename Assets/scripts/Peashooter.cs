442076ceca3ca426491bf1b779d473ff59938542
Zero Y One K <markbillhv@gmail.com>
2024-12-02 02:48:33 +0700

    refining bullet + making health bar

--- Assets/scripts/Peashooter.cs
+++ Assets/scripts/Peashooter.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Peashooter : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+
+
     //shoot
     public bool isShootable = false;
     public GameObject bullet;
