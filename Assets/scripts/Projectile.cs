04c76c9ed9f4ae79e0184fbaab8087532c5a6653
Zero Y One K <markbillhv@gmail.com>
2024-12-04 06:17:17 +0700

    k nho gi nua chi nho ten em

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -31,7 +31,8 @@ public class Projectile : MonoBehaviour
         boxCollider.enabled = false;
         anim.SetTrigger("explode");
 
-        
+        if (collision.tag == "Enemy")
+            collision.GetComponent<Health>().TakeDamage(1);
     }
     public void SetDirection(float _direction)
     {
