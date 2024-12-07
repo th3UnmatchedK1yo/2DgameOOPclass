3900e34718f12c861439d71e5a36f830649bba04
Zero Y One K <markbillhv@gmail.com>
2024-12-07 18:06:39 +0700

    travel back time

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip checkpoint;
+    public AudioClip wallTouch;
+    public AudioClip jump;
+    public AudioClip bullet;
+
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
--- Assets/scripts/Collector.cs
+++ Assets/scripts/Collector.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
+    [Header("Collect Sound")]
+    [SerializeField] private AudioClip collectSound; // Reference to the collect sound
+    private AudioSource audioSource; // Reference to AudioSource
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Item item = collision.GetComponent<Item>();
-        if(item!= null)
+        if (item != null)
         {
             item.Collect();
+            SoundManager.instance.PlaySound(collectSound);
+
+
         }
     }
 }
