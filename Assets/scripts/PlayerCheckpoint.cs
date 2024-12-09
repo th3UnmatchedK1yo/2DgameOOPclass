241123f5ff6bd94a905b089d96af05394a70a793
Zero Y One K <markbillhv@gmail.com>
2024-12-09 19:10:01 +0700

    remove mask dude

---
+++ Assets/scripts/PlayerCheckpoint.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] private AudioClip checkpoint;
+    private Transform currentCheckpoint;
+    
+
+    
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Checkpoint")
+        {
+            currentCheckpoint = collision.transform;
+            SoundManager.instance.PlaySound(checkpoint);
+            collision.GetComponent<Collider2D>().enabled = false;
+            collision.GetComponent<Animator>().SetTrigger("activate");
+        }
+    }
+}
